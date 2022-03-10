3fc28db2ac7be268d69d4a88fa9c33a37e9910db
ArtemZorin36 <artem-zorin@bk.ru>
2022-03-10 23:46:23 +0300

    fix

--- src/test/java/severstal/helpers/BaseState.java
+++ src/test/java/severstal/helpers/BaseState.java
@@ -3,23 +3,27 @@ package severstal.helpers;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import severstal.config.Credentials;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static java.lang.String.format;
 
 public class BaseState {
 
     @BeforeAll
     public static void beforeAll() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-        ConfigServer config = ConfigFactory.create(ConfigServer.class);
 
         Configuration.browserSize = "1920x1080";
-//        Configuration.browser = System.getProperty("browser");
-        Configuration.remote = config.hostname();
+        String user = Credentials.config.user();
+        String password = Credentials.config.password();
+        String url = System.getProperty("url");
+        String remoteUrl = format("https://%s:%s@%s", user, password, url);
+
+        Configuration.remote = remoteUrl;
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
